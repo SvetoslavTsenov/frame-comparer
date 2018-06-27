12a8a50f5ac30003d1e4581f34c9e803fe663a96
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-06-28 00:25:34 +0300

    refactor: extend process video

--- lib/frame-comparer.ts
+++ lib/frame-comparer.ts
@@ -5,22 +5,15 @@ import { statSync, existsSync, readdirSync, unlinkSync, rmdirSync, mkdirSync, wr
 import { IRectangle } from './interfaces/rectangle';
 
 export class FrameComparer {
-    private _frameStorageFullName: string;
-    private _frames: Array<string>;
-
-    public async compareImageFromVideo(expectedImageFullName: string, logStorage: string, startRange, endRange, tollerance: number = 0.2, saveActualImageAsExpected: boolean = true, shouldLogImageResults: boolean = false, cropImageRect: IRectangle = undefined, verbose = true): Promise<boolean> {
+    public static async compareImageFromVideo(frames: Array<string>, expectedImageFullName: string, logStorage: string, startRange, endRange, tollerance: number = 0.2, saveActualImageAsExpected: boolean = true, shouldLogImageResults: boolean = false, cropImageRect: IRectangle = undefined, verbose = true): Promise<boolean> {
         if (!saveActualImageAsExpected && !existsSync(expectedImageFullName)) {
             throw new Error(`${expectedImageFullName} is not available!!!`);
         }
         return new Promise<boolean>(async (accept, reject) => {
-            endRange = endRange < this._frames.length ? endRange : this._frames.length;
-            const filteredFrames = this._frames.filter(f => {
-                const number = f.replace(/\D/g, "");
-                if (number >= startRange && number <= endRange) {
-                    return true;
-                }
-
-                return false;
+            endRange = endRange < frames.length ? endRange : frames.length;
+            const filteredFrames = frames.filter(f => {
+                const number = FrameComparer.convertFrameNameToNumber(f);
+                return number >= startRange && number <= endRange;
             });
 
             let isExcpectedImageAvailable = true;
@@ -41,7 +34,7 @@ export class FrameComparer {
 
                 if (isExcpectedImageAvailable) {
                     const diffImage = (!shouldLogImageResults || !logStorage) ? undefined : resolve(logStorage, basename(filteredFrames[index].replace(extn, `_diff${extn}`)));
-                    const result = await this.compareImages(filteredFrames[index], expectedImageFullName, diffImage, tollerance, blinkDiff.THRESHOLD_PERCENT, cropImageRect, verbose);
+                    const result = (await FrameComparer.compareImages(filteredFrames[index], expectedImageFullName, diffImage, tollerance, blinkDiff.THRESHOLD_PERCENT, cropImageRect, verbose)).result;
                     if (result) {
                         return accept(true);
                     }
@@ -52,14 +45,59 @@ export class FrameComparer {
         });
     }
 
-    public processVideo(fullVideoName: string, frameStorageFullName = "tempFramesFolder", framesGeneralName = "frame") {
-        this._frameStorageFullName = frameStorageFullName;
-        this.cleanDir(this._frameStorageFullName);
-        mkdirSync(this._frameStorageFullName);
+    public static getEqualImages(frames, expectedImageFullName: string, startRange: number, endRange: number, tollerance: number = 0.00, cropImageRect: IRectangle = undefined, verbose = true): Promise<Map<number, string>> {
+        if (!existsSync(expectedImageFullName)) {
+            throw new Error(`${expectedImageFullName} is not available!!!`);
+        }
+        return new Promise<Map<number, string>>(async (accept, reject) => {
+            endRange = endRange < frames.length ? endRange : frames.length -1;
+            const filteredFrames = frames.filter(f => {
+                const number = FrameComparer.convertFrameNameToNumber(f);
+                if (number >= startRange && number <= endRange) {
+                    return true;
+                }
+
+                return false;
+            }).sort((a, b) => {
+                const numberA = FrameComparer.convertFrameNameToNumber(a);
+                const numberB = FrameComparer.convertFrameNameToNumber(b);
+
+                return numberA > numberB ? 1 : -1;
+            });
+
+            const equalFrames = new Map<number, string>();
+            let previosResult = undefined;
+            for (let index = 0; index < filteredFrames.length; index++) {
+                if (verbose) {
+                    console.log(filteredFrames[index]);
+                }
+
+                const ext = extname(filteredFrames[index]);
+                const diffImageName = verbose ? filteredFrames[index].replace(ext, `_diff${ext}`) : undefined;
+                const result = await FrameComparer.compareImages(filteredFrames[index], expectedImageFullName, diffImageName, tollerance, blinkDiff.THRESHOLD_PIXEL, cropImageRect, verbose);
+                
+                // if(!result.result && result.differences - previosResult > 40000 ){
+                //     index = filteredFrames.length;
+                // }
+                previosResult = result.differences;
+                if (result.result) {
+                    const number = FrameComparer.convertFrameNameToNumber(filteredFrames[index]);
+                    equalFrames.set(number, filteredFrames[index]);
+                }
+            }
+
+            return accept(equalFrames);
+        });
+    }
+
+    public static processVideo(fullVideoName: string, frameStorageFullName = "tempFramesFolder", framesGeneralName = "frame") {
+        FrameComparer.cleanDir(frameStorageFullName);
+        if (!existsSync(frameStorageFullName)) {
+            mkdirSync(frameStorageFullName);
+        }
         let lastFrameEnqueued = 0;
-        const sorage = this._frameStorageFullName;
-        const that = this;
-        const imageName = resolve(this._frameStorageFullName, framesGeneralName)
+        const frames = new Array();;
+        const imageName = resolve(frameStorageFullName, framesGeneralName)
         return new Promise<any>((res, reject) => {
             ffmpeg(fullVideoName)
                 .on('error', function (err) {
@@ -67,15 +105,14 @@ export class FrameComparer {
                     reject();
                 })
                 .on('end', function () {
-                    that._frames = new Array();
                     console.log('Processing finished !');
-                    readdirSync(sorage)
+                    readdirSync(frameStorageFullName)
                         .forEach((file) => {
-                            file = resolve(sorage, file);
-                            that._frames.push(file);
+                            file = resolve(frameStorageFullName, file);
+                            frames.push(file);
                         });
 
-                    res();
+                    return res(frames);
                 })
                 .on('progress', function (progress) {
                     lastFrameEnqueued = progress.frames - 1;
@@ -87,7 +124,24 @@ export class FrameComparer {
         });
     }
 
-    private async compareImages(actual: string, expected: string, output: string, valueThreshold: number = 0.01, typeThreshold, cropImageRect: IRectangle, verbose = true) {
+    public static async getMetaData(videoFullName: string) {
+        return new Promise<any>((resolve, reject) => {
+            ffmpeg.ffprobe(videoFullName, function (err, metadata) {
+                //console.dir(metadata); // all metadata
+                console.log("Meta data info: ", metadata);
+                if (err) {
+                    console.error(err);
+                }
+                resolve(metadata);
+            });
+        });
+    }
+
+    private static convertFrameNameToNumber = frame => {
+        return parseInt(/\d+/gm.exec(/\d+.png/gm.exec(frame)[0])[0]);
+    }
+
+    private static async compareImages(actual: string, expected: string, output: string, valueThreshold: number = 0.01, typeThreshold, cropImageRect: IRectangle, verbose = true) {
         const diff = new blinkDiff({
             imageAPath: actual,
             cropImageA: cropImageRect,
@@ -100,11 +154,11 @@ export class FrameComparer {
             delta: 20,
         });
 
-        return await this.runDiff(diff, verbose);
+        return await FrameComparer.runDiff(diff, verbose);
     }
 
-    private runDiff(diffOptions: blinkDiff, verbose) {
-        return new Promise<boolean>((resolve, reject) => {
+    private static runDiff(diffOptions: blinkDiff, verbose) {
+        return new Promise<{ differences: number, result: boolean }>((resolve, reject) => {
             diffOptions.run(function (error, result) {
                 if (error) {
                     throw error;
@@ -117,37 +171,38 @@ export class FrameComparer {
                             console.log(message);
                             console.log('Found ' + result.differences + ' differences.');
                         }
-                        return resolve(true);
+                        return resolve({ differences: result.differences, result: true });
                     } else {
                         if (verbose) {
                             message = "Screen compare failed!";
                             console.log(message);
                             console.log('Found ' + result.differences + ' differences.');
                         }
-                        return resolve(false);
+                        return resolve({ differences: result.differences, result: false });
                     }
                 }
             });
         });
     }
 
-    private cleanDir(dirFullName) {
+    private static cleanDir(dirFullName) {
         if (existsSync(dirFullName)) {
             const pathToFile = dirFullName;
             readdirSync(dirFullName)
                 .forEach(file => {
                     const f = resolve(pathToFile, file);
-                    if (this.isDirectory(f)) {
-                        this.cleanDir(f);
+                    if (FrameComparer.isDirectory(f)) {
+                        FrameComparer.cleanDir(f);
                     }
                     unlinkSync(f);
                 });
 
+            //
             rmdirSync(dirFullName);
         }
     }
 
-    private isDirectory(fullName) {
+    private static isDirectory(fullName) {
         try {
             if (existsSync(fullName) && statSync(fullName).isDirectory()) {
                 return true;
@@ -159,3 +214,4 @@ export class FrameComparer {
         return false;
     }
 }
+
